32a0c28a9b08edb0e062950f00fa9054e256b890
Jose Manuel Rodriguez <jose.m.rodriguez@yukisoftware.es>
2021-09-16 14:58:52 +0200

    vue router guard

--- 05-rutas-ciclo/src/modules/pokemon/pages/PokemonPage.vue
+++ 05-rutas-ciclo/src/modules/pokemon/pages/PokemonPage.vue
@@ -1,5 +1,5 @@
 <template>
-    <h1>Pokemon page</h1>
+    <h1>Pokemon: <span>#{{ id }}</span> </h1>
     <div v-if="pokemon">
         <img :src="pokemon.sprites.front_default" :alt="pokemon.name">
     </div>
@@ -7,35 +7,46 @@
 
 <script>
 export default {
-
     props: {
         id: {
             type: Number,
-            required: true,
-        }
+            required: true
+        },
     },
+
     data() {
         return {
+            // id: this.$route.params.id,
             pokemon: null
         }
     },
+    
     created() {
+
+        // const { id } = this.$route.params
+        // console.log(id)
+        // this.id = id
+        // console.log(this.$attrs)
         this.getPokemon()
     },
     methods: {
         async getPokemon() {
-            try {      
-                const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${this.id}`).then( r => r.json())
+            try {
+                const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${ this.id }`).then( r => r.json() );
+                console.log(pokemon)
                 this.pokemon = pokemon
+
             } catch (error) {
                 this.$router.push('/')
+                console.log('No hay nada que hacer aquí')
             }
         }
     },
     watch: {
         id() {
-            this.getPokemon();
+            this.getPokemon()
         }
     }
+
 }
 </script>
