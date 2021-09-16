0743f89ec696b873952f6456ff9f3001901d93ab
Jose Manuel Rodriguez <jose.m.rodriguez@yukisoftware.es>
2021-09-16 14:26:55 +0200

    vue routes section

---
+++ 05-rutas-ciclo/src/modules/pokemon/pages/PokemonPage.vue
@@ -0,0 +1,41 @@
+<template>
+    <h1>Pokemon page</h1>
+    <div v-if="pokemon">
+        <img :src="pokemon.sprites.front_default" :alt="pokemon.name">
+    </div>
+</template>
+
+<script>
+export default {
+
+    props: {
+        id: {
+            type: Number,
+            required: true,
+        }
+    },
+    data() {
+        return {
+            pokemon: null
+        }
+    },
+    created() {
+        this.getPokemon()
+    },
+    methods: {
+        async getPokemon() {
+            try {      
+                const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${this.id}`).then( r => r.json())
+                this.pokemon = pokemon
+            } catch (error) {
+                this.$router.push('/')
+            }
+        }
+    },
+    watch: {
+        id() {
+            this.getPokemon();
+        }
+    }
+}
+</script>
---
+++ 05-rutas-ciclo/src/modules/shared/components/NavBar.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <router-link to="/">Pokemon list</router-link>
+        <router-link :to="{name: 'pokemon-id', params:{ id: 25}}">Pokemon ID</router-link>
+        <router-link to="/about">About</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    div {
+        padding: 0 30px;
+    }
+
+    div a {
+        font-weight: bold;
+        color: #2c3e50;
+        margin: 0 10px;
+    }
+
+    div .router-link-exact-active {
+        color: #42b983
+    }
+</style>
